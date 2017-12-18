f26e1489feffcfb86ac5c9ddd8ceba3eaf85abeb
Tri Doan <tridoan1998@gmail.com>
2017-12-18 01:06:52 -0800

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,17 @@
+#EEKDAYS = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+
+#for i, value in enumerate( WEEKDAYS, 1):
+    #print(i, value )
+
+
+def fibonacci(n):
+    print("sdfsfd")
+    if n == 0:
+        return 0
+    return fibonacci(n - 1)
+
+print(fibonacci(10))
+        #https://pythonspot.com/
+        #https://www.tutorialspoint.com/python/python_basic_operators.htm
+
+
